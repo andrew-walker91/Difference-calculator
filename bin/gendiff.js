3afc10e78f15327511d5cb4e0fb02c15f926d9d5
Andrew Walker <gulyaykin@gmail.com>
2022-12-06 11:09:27 +0300

    add functions to commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.format));
+  });
 
 program.parse();
