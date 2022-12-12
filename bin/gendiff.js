034a40bf12e65e3034fb908ababbc5a222eed126
Andrew Walker <gulyaykin@gmail.com>
2022-12-12 19:48:38 +0300

    update programm

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,7 @@ program
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2, program.format));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
