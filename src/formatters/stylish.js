46428dfa94052b5e122a901f8ebeae3e3cd3bdd9
Andrew Walker <gulyaykin@gmail.com>
2022-12-14 20:09:32 +0300

    rename consts, refact funcs

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
-const depthIndent = (depth) => '    '.repeat(depth);
-const plusGap = '  + ';
-const minusGap = '  - ';
-const neutralGap = '    ';
+const indent = (depth) => '    '.repeat(depth);
+const plus = '  + ';
+const minus = '  - ';
+const neutral = '    ';
 
 // Вспомогательная функция для обработки значений
 const valueFormatter = (value, depth) => {
@@ -12,33 +12,36 @@ const valueFormatter = (value, depth) => {
   }
 
   const keys = Object.keys(value);
-  const formattedValue = keys.map((key) => `${depthIndent(depth + 2)}${key}: ${valueFormatter(value[key], depth + 1)}`);
-  return `{\n${formattedValue.join('\n')}\n${depthIndent(depth + 1)}}`;
+  const formattedValue = keys.map((key) => `${indent(depth + 2)}${key}: ${valueFormatter(value[key], depth + 1)}`);
+  return `{\n${formattedValue.join('\n')}\n${indent(depth + 1)}}`;
 };
 
 // Функция формирует stylish-вывод в консоль на основе результата из genDiff
 const formatter = (data) => {
   const innerFormatter = (innerData, depth = 0) => {
-    const formattedData = innerData.map((node) => {
+    const formattedData = innerData.flatMap((node) => {
       if (node.type === 'ADDED') {
-        return `${depthIndent(depth)}${plusGap}${node.name}: ${valueFormatter(node.value, depth)}`;
+        return `${indent(depth)}${plus}${node.name}: ${valueFormatter(node.value, depth)}`;
       }
       if (node.type === 'REMOVED') {
-        return `${depthIndent(depth)}${minusGap}${node.name}: ${valueFormatter(node.value, depth)}`;
+        return `${indent(depth)}${minus}${node.name}: ${valueFormatter(node.value, depth)}`;
       }
       if (node.type === 'UNCHANGED') {
-        return `${depthIndent(depth)}${neutralGap}${node.name}: ${valueFormatter(node.value, depth)}`;
+        return `${indent(depth)}${neutral}${node.name}: ${valueFormatter(node.value, depth)}`;
       }
       if (node.type === 'CHANGED') {
-        return `${depthIndent(depth)}${minusGap}${node.name}: ${valueFormatter(node.oldValue, depth)}\n${depthIndent(depth)}${plusGap}${node.name}: ${valueFormatter(node.newValue, depth)}`;
+        return [
+          `${indent(depth)}${minus}${node.name}: ${valueFormatter(node.oldValue, depth)}`,
+          `${indent(depth)}${plus}${node.name}: ${valueFormatter(node.newValue, depth)}`,
+        ];
       }
       if (node.type === 'PARENT') {
-        return `${depthIndent(depth)}${neutralGap}${node.name}: ${innerFormatter(node.children, depth + 1)}`;
+        return `${indent(depth)}${neutral}${node.name}: ${innerFormatter(node.children, depth + 1)}`;
       }
       throw new Error(`"${node.type}" type is not supported by the formatter`);
     });
 
-    return `{\n${formattedData.join('\n')}\n${depthIndent(depth)}}`;
+    return `{\n${formattedData.join('\n')}\n${indent(depth)}}`;
   };
 
   return innerFormatter(data);
