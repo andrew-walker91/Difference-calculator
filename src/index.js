45eaabfea8367ab96862cd4e1af59af595b9b6be
Andrew Walker <gulyaykin@gmail.com>
2022-12-14 20:10:55 +0300

    add abstractions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-import fs from 'fs';
+import { readFileSync } from 'node:fs';
 import path from 'path';
 import genDiff from './gendiff.js';
 import parser from './parser.js';
 import format from './formatters/index.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  // Преобразуем пути до файлов в абсолютные
-  const absolutePath1 = path.resolve(process.cwd(), filepath1);
-  const absolutePath2 = path.resolve(process.cwd(), filepath2);
-
-  // Выясняем формат файлов
-  const dataFormat1 = path.extname(absolutePath1).slice(1);
-  const dataFormat2 = path.extname(absolutePath2).slice(1);
-
-  // Читаем файлы
-  const data1 = fs.readFileSync(absolutePath1);
-  const data2 = fs.readFileSync(absolutePath2);
+  const getData = (filepath) => readFileSync(path.resolve(process.cwd(), filepath));
+  const getExtension = (filepath) => path.extname(filepath).slice(1);
 
   // Парсим файлы в JS-объекты
-  const obj1 = parser(data1, dataFormat1);
-  const obj2 = parser(data2, dataFormat2);
+  const obj1 = parser(getData(filepath1), getExtension(filepath1));
+  const obj2 = parser(getData(filepath2), getExtension(filepath2));
 
   // Формируем diff-файл
   const diff = genDiff(obj1, obj2);
